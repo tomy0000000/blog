dc1914e0bc1289e705f6b559da9121bdd580f664
Tomy Hsieh <git@tomy.me>
2024-01-02 13:37:57 +0800

    🗄 chore: Handful script to generate ignore list

---
+++ scripts/generate_vscode_ignore.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+import json5
+import json
+import frontmatter
+
+PROJECT_ROOT = Path(__file__).parent.parent
+PROJECT_SETTINGS = PROJECT_ROOT / ".vscode" / "settings.json"
+
+def keep(post_dir: Path) -> bool:
+    drafts = post_dir.glob("index.*.md")
+    for draft in drafts:
+        meta = frontmatter.load(draft)
+        if "draft" in meta and meta["draft"] == True:
+            return True
+    return False
+
+def update_ignore_list(ignored: dict) -> None:
+    with open(PROJECT_SETTINGS, "r") as f:
+        settings = json5.load(f)
+
+    if "files.exclude" not in settings:
+        settings["files.exclude"] = {}
+
+    for existing in settings["files.exclude"]:
+        if not existing.startswith("content/posts/"):
+            ignored[f"{existing}"] = settings["files.exclude"][existing]
+
+    settings["files.exclude"] = {}
+    for post in ignored:
+        settings["files.exclude"][post] = True
+
+    with open(PROJECT_SETTINGS, "w") as f:
+        json.dump(settings, f, indent=2, sort_keys=True)
+
+def main() -> None:
+    posts = list(PROJECT_ROOT.glob("content/posts/*"))
+    posts.sort()
+    ignored = {}
+    for post in posts:
+        if not keep(post):
+            print(f"[IGNORED] {post.name}")
+            ignored[str(post.relative_to(PROJECT_ROOT))] = True
+        else:
+            print(f"[KEEP   ] {post.name}")
+    update_ignore_list(ignored)
+
+if __name__ == "__main__":
+    main()
